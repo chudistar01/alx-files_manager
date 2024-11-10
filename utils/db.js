7317a93b5b39f1d72f58349f58283b3fa71ceee5
chudistar01 <christianigwebuike31@gmail.com>
2024-11-10 11:28:33 -0800

    file manager

--- utils/db.js
+++ utils/db.js
@@ -9,44 +9,44 @@ const url = `mongodb://${DB_HOST}:${DB_PORT}`;
  * Class
  */
 class DBClient {
-    constructor() {
-	MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-	    if (!err) {
-		this.db = client.db(DB_DATABASE);
-		this.usersCollection = this.db.collection('users');
-		this.filesCollection = this.db.collection('files');
-	    } else {
-		console.log(err.message);
-		this.db = false;
-	    }
-	});
-    }
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
 
-    /**
+  /**
      * check if connection is alive
      * @return return
      */
-    isAlive() {
-	return Boolean(this.db);
-    }
+  isAlive() {
+    return Boolean(this.db);
+  }
 
-    /**
+  /**
      * returns the number of documents
      * @return number of users
      */
-    async nbUsers() {
-	const numberOfUsers = this.usersCollection.countDocuments();
-	return numberOfUsers;
-    }
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
 
-    /**
+  /**
      * returns the number of documents
      * @return number of files
      */
-    async nbFiles() {
-	const numberOfFiles = this.filesCollection.countDocuments();
-	return numberOfFiles;
-    }
+  async nbFiles() {
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
+  }
 }
 
 const dbClient = new DBClient();
