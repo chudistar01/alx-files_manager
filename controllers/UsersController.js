da49bf9d4012bf9c9aa2230840309712ddf00ba7
chudistar01 <christianigwebuike31@gmail.com>
2024-11-10 22:10:49 -0800

    file manager

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,4 @@
-import {ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import Queue from 'bull';
 import dbClient from '../utils/db';
@@ -6,47 +6,45 @@ import dbClient from '../utils/db';
 const userQueue = new Queue('userQueue');
 
 class UsersController {
-    /**
+  /**
      * handles users registeration
      */
-    static async postNew(request, response) {
-	const { email, password } = request.body;
+  static async postNew(request, response) {
+    const { email, password } = request.body;
 
-	if (!email) return response.status(400).send({ error: 'Missing email' });
+    if (!email) return response.status(400).send({ error: 'Missing email' });
 
-	if (!password) { return response.status(400).send({ error: 'Missing password' }); }
+    if (!password) { return response.status(400).send({ error: 'Missing password' }); }
 
-	const emailExists = await dbClient.usersCollection.findOne({ email });
+    const emailExists = await dbClient.usersCollection.findOne({ email });
 
-	if (emailExists) { return response.status(400).send({ error: 'Already exist' }); }
+    if (emailExists) { return response.status(400).send({ error: 'Already exist' }); }
 
-	const sha1Password = sha1(password);
+    const sha1Password = sha1(password);
 
-	let result;
+    let result;
 
-	try {
-	    result = await dbClient.usersCollection.insertOne({
-		email,
-		password: sha1Password,
-	    });
-	} catch (err) {
-	    await userQueue.add({});
-	    return response.status(500).send({ error: 'Error creating user.' });
-	}
-
-	const user = {
-	    id: result.insertedId,
-	    email,
-	};
-
-	await userQueue.add({
-	    userId: result.insertedId.toString(),
-	});
-
-	return response.status(201).send(user);
+    try {
+      result = await dbClient.usersCollection.insertOne({
+        email,
+        password: sha1Password,
+      });
+    } catch (err) {
+      await userQueue.add({});
+      return response.status(500).send({ error: 'Error creating user.' });
     }
 
-	
+    const user = {
+      id: result.insertedId,
+      email,
+    };
+
+    await userQueue.add({
+      userId: result.insertedId.toString(),
+    });
+
+    return response.status(201).send(user);
+  }
 }
 
 export default UsersController;
